86c7ebf91c991c083f9307b656d256766a1c2bd4
Inrixia <Inrixia@gmail.com>
2019-03-03 16:01:39 +1300

    Add files via upload

--- fimSearch/searchEngine.js
+++ fimSearch/searchEngine.js
@@ -1,30 +1,44 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 80;
-var MongoClient = require('mongodb').MongoClient;
+var port = process.env.PORT || 25566;
 var url = "REDACTED";
+const elasticsearch = require('elasticsearch');
+const client = new elasticsearch.Client({
+	requestTimeout: 120000,
+	hosts: ['http://localhost:9200']
+});
 
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', function(socket){
+  socket.on('search', function(msg){
+  	var searchBody = {
+		"_source": ["title", "cover_image", "short_description", "content_rating", "url", "author", "num_views", "num_words", "tags"], 
+		"query": {
+			"match_phrase": {
+				"story.fullStoryText": { 
+					"query": msg,
+					"analyzer": "search_analis",
+					"slop": 2
+				}
+			}
+		},
+		"highlight": {
+			"fields": {
+				"story.fullStoryText": {}
+			}
+		}
+	};
+	client.search({index: 'fimarchive', body: searchBody}).then(data => {
+		io.emit('search_result', data);
+	})
+  });
+});
 
-MongoClient.connect(url, function(err, db) {
-  	if (err) throw err;
-  	console.log("Database opened!");
-
-  	app.get('/', function(req, res){
-	  res.sendFile(__dirname + '/index.html');
-	});
-
-	io.on('connection', function(socket){
-	  socket.on('search', function(msg){
-	  	db.db("spookelton").collection('fim').find( { $text: { $search: msg }}).project({ title: 1, description_html : 1, cover_image: 1, content_rating: 1, url: 1 }).limit(10).toArray(function(err, result) {
-	    	io.emit('search_result', result);
-		});
-	  });
-	});
-
-	http.listen(port, function(){
-  		console.log('listening on *:' + port);
-	});
+http.listen(port, function(){
+	console.log('listening on *:' + port);
 });
