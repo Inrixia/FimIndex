bb90e40c3e23cf2552fae736bcc96d0b38ab5f98
Inrixia <Inrixia@gmail.com>
2019-03-03 17:35:19 +1300

    Add files via upload

--- fimSearch/searchEngine.js
+++ fimSearch/searchEngine.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 25566;
+var port = process.env.PORT || 8585;
 var url = "REDACTED";
 const elasticsearch = require('elasticsearch');
 const client = new elasticsearch.Client({
@@ -14,22 +14,38 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
+function elasticEscape(string) {
+	return string.replace(/[\*\+\-=~><\"\?^\${}\(\)\:\!\/[\]\\\s]/g, '\\$&') // replace single character special characters
+    .replace(/\|\|/g, '\\||') // replace ||
+    .replace(/\&\&/g, '\\&&') // replace &&
+    .replace(/AND/g, '\\A\\N\\D') // replace AND
+    .replace(/OR/g, '\\O\\R') // replace OR
+    .replace(/NOT/g, '\\N\\O\\T'); // replace NOT
+}
+
 io.on('connection', function(socket){
-  socket.on('search', function(msg){
+  socket.on('search', function(searchObject){
+  	var searchString = elasticEscape(searchObject.value);
+  	var searchSlop = elasticEscape(searchObject.slop);
+  	console.log(searchString, searchSlop)
   	var searchBody = {
 		"_source": ["title", "cover_image", "short_description", "content_rating", "url", "author", "num_views", "num_words", "tags"], 
 		"query": {
 			"match_phrase": {
 				"story.fullStoryText": { 
-					"query": msg,
+					"query": searchString,
 					"analyzer": "search_analis",
-					"slop": 2
+					"slop": searchSlop
 				}
 			}
 		},
 		"highlight": {
 			"fields": {
-				"story.fullStoryText": {}
+				"story.fullStoryText": {
+					"pre_tags": "<b style='background-color: hsla(0, 100%, 50%, 0.4);'>",
+        			"post_tags": "</b>"
+				}
 			}
 		}
 	};
@@ -38,6 +54,7 @@ io.on('connection', function(socket){
 	})
   });
 });
+
 
 http.listen(port, function(){
 	console.log('listening on *:' + port);
